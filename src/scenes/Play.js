d21b8ff691a550dcb81b18cf18621107b08b9d69
Cromwell <cdeguzman1208@gmail.com>
2023-04-18 16:44:02 -0700

    shooting spaceships

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -8,6 +8,9 @@ class Play extends Phaser.Scene {
         this.load.image('rocket', './assets/rocket.png');
         this.load.image('spaceship', './assets/spaceship.png');
         this.load.image('starfield', './assets/starfield.png');
+
+        // load spritesheet
+        this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
     }
 
     create() {
@@ -35,6 +38,13 @@ class Play extends Phaser.Scene {
         this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 30).setOrigin(0, 0);
         this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0,0);
         this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0,0);
+
+        // animation config
+        this.anims.create({
+            key: 'explode',
+            frames: this.anims.generateFrameNumbers('explosion', { start: 0, end: 9, first: 0}),
+            frameRate: 30
+        });
     }
 
     update() {
@@ -46,16 +56,22 @@ class Play extends Phaser.Scene {
 
         // check collisions
         if(this.checkCollision(this.p1Rocket, this.ship03)) {
+            // console.log('kaboom ship 03');
             this.p1Rocket.reset();
-            console.log('kaboom ship 03');
+            // this.ship03.reset();
+            this.shipExplode(this.ship03);
         }
         if (this.checkCollision(this.p1Rocket, this.ship02)) {
+            // console.log('kaboom ship 02');
             this.p1Rocket.reset();
-            console.log('kaboom ship 02');
+            // this.ship02.reset();
+            this.shipExplode(this.ship02);
         }
         if (this.checkCollision(this.p1Rocket, this.ship01)) {
+            // console.log('kaboom ship 01');
             this.p1Rocket.reset();
-            console.log('kaboom ship 01');
+            // this.ship01.reset();
+            this.shipExplode(this.ship01);
         }
     }
 
@@ -66,5 +82,19 @@ class Play extends Phaser.Scene {
         } else {
             return false;
         }
+    }
+
+    shipExplode(ship) {
+        // temporarily hide ship
+        ship.alpha = 0;
+
+        // create explosion sprite at ship's position
+        let boom = this.add.sprite(ship.x, ship.y, 'explosion').setOrigin(0, 0);
+        boom.anims.play('explode'); // play explode animation
+        boom.on('animationcomplete', () => { // callback after anim completes
+            ship.reset(); // reset ship position
+            ship.alpha = 1; // make ship visible again
+            boom.destroy(); // remove explosion sprite
+        });
     }
 }
