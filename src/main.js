788fc9fe8842fa17ee749658fa306c854164ac4c
Cromwell <cdeguzman1208@gmail.com>
2023-04-27 15:09:38 -0700

    update header

--- src/main.js
+++ src/main.js
@@ -8,14 +8,14 @@ Mods:
 - Add your own (copyright-free) background music to the Play scene (please be mindful of the volume) (5)
 - Implement the speed increase that happens after 30 seconds in the original game (5)
 - Randomize each spaceship's movement direction at the start of each play (5)
-- Create a new scrolling tile sprite for the background (5)
+> Create a new scrolling tile sprite for the background (5)
 - Allow the player to control the Rocket after it's fired (5)
 - Create 4 new explosion sound effects and randomize which one plays on impact (10)
 - Display the time remaining (in seconds) on the screen (10)
-- Using a texture atlas, create a new animated sprite for the Spaceship enemies (10)
+> Using a texture atlas, create a new animated sprite for the Spaceship enemies (10)
 - Create a new title screen (e.g., new artwork, typography, layout) (10)
 - Create a new enemy Spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (15)
-x Use Phaser's particle emitter to create a particle explosion when the rocket hits the spaceship (15)
+> Use Phaser's particle emitter to create a particle explosion when the rocket hits the spaceship (15)
 
 Citations:
 - Beatrice Yu (CMPM 120-01)
